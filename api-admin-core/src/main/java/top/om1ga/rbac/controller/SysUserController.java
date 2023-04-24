9d621d696695d7b39771474f00d38c7ac00aaffc
om1ga <845085139@qq.com>
2023-04-24 15:57:02 +0800

    feat(custom): 用户修改密码接口

--- api-admin-core/src/main/java/top/om1ga/rbac/controller/SysUserController.java
+++ api-admin-core/src/main/java/top/om1ga/rbac/controller/SysUserController.java
@@ -9,14 +9,19 @@ package top.om1ga.rbac.controller;
  */
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.om1ga.common.utils.Result;
 import top.om1ga.rbac.convert.SysUserConvert;
 import top.om1ga.rbac.service.SysMenuService;
+import top.om1ga.rbac.service.SysUserService;
 import top.om1ga.rbac.vo.SysAuthVO;
+import top.om1ga.rbac.vo.SysUserPasswordVO;
 import top.om1ga.rbac.vo.SysUserVO;
 import top.om1ga.security.user.SecurityUser;
 import top.om1ga.security.user.UserDetail;
@@ -32,6 +37,8 @@ import top.om1ga.security.user.UserDetail;
 @Tag(name = "用户管理")
 public class SysUserController {
     private final SysMenuService sysMenuService;
+    private final PasswordEncoder passwordEncoder;
+    private final SysUserService sysUserService;
 
     @PostMapping("info")
     @Operation(summary = "获取登录用户信息")
@@ -46,5 +53,18 @@ public class SysUserController {
         //3 获得用户授权信息
         vo.setAuthority(sysMenuService.getUserAuthority(userDetail));
         return Result.ok(vo);
+    }
+
+    @PostMapping("password")
+    @Operation(summary = "修改密码")
+    public Result<String> password(@RequestBody @Valid SysUserPasswordVO vo) {
+        // 原密码不正确
+        UserDetail user = SecurityUser.getUser();
+        if (!passwordEncoder.matches(vo.getOldPassword(), user.getPassword())) {
+            return Result.error("原密码不正确");
+        }
+        // 修改密码
+        sysUserService.updatePassword(user.getId(), passwordEncoder.encode(vo.getNewPassword()));
+        return Result.ok();
     }
 }
